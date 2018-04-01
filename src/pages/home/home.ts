1f5d6bd853c2952fc32996bd392da126fa6d2d87
anushyapriya <arshin.28@gmail.com>
2018-04-01 23:08:43 +0530

    bar code scaner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedProduct : any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner) {
 
   }
 
+  scan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.selectedProduct = barcodeData;
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+  
+   
 }

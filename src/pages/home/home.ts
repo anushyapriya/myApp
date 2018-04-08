1a4c17aa96786189b805db5499e8ab84da66f561
anushyapriya <arshin.28@gmail.com>
2018-04-08 22:07:36 +0530

    added format

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,15 +13,27 @@ export class HomePage {
   jsonFormat:any;
   showResults:boolean= false;
   
-  results=[
-    {title:"Name",icon:"person",text:"Eswar"},
-    {title:"Address",icon:"home",text:"D.no 74-6/2-1B, plot no 132, Chennai , Tamilnaidu-600303"},
-    {title:"Phone Number",icon:"call",text:"+91-999999999"},    
-    
-    
-  ];
+  results = [];
   constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner,
     private geolocation: Geolocation) {
+      
+    
+    
+  }
+
+  scan(){
+    this.results=[];
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.selectedProduct = JSON.stringify(barcodeData);
+      if(barcodeData.format ==='CODE_39'){
+      this.results=[
+        {title:"Name",icon:"person",text:barcodeData.text},
+        {title:"Address",icon:"home",text:"D.no 74-6/2-1B, plot no 132, Chennai , Tamilnaidu-600303"},
+        {title:"Phone Number",icon:"call",text:"+91-999999999"},    
+        
+        
+      ];
       this.geolocation.getCurrentPosition().then((resp) => {
         console.log(resp);
         var location = resp.coords.latitude.toString()+", "+resp.coords.longitude.toString();
@@ -30,15 +42,8 @@ export class HomePage {
        }).catch((error) => {
          console.log('Error getting location', error);
        });
-    
-    
-  }
-
-  scan(){
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-      this.selectedProduct = JSON.stringify(barcodeData);
       this.showResults = true;
+      }
      /*  var parseString = xml2js.parseString;
       
       parseString(barcodeData, function (err, result) {
